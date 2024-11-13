ea602824a8ae1cf04471220cb1e737710acd8821
AnirudhGirish <anirudhgirish08@gmail.com>
2024-11-14 01:13:58 +0530

    Everything done now Debugging Time

--- ReactBlog/src/main.jsx
+++ ReactBlog/src/main.jsx
@@ -4,11 +4,80 @@ import './index.css'
 import App from './App.jsx'
 import {Provider} from 'react-redux'
 import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AuthLayout, Login} from './components/index.js'
+import Signup from './pages/Signup.jsx'
+import Home from './pages/Home.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AddPost from './pages/AddPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/login',
+        element:(
+        <AuthLayout authentiaction={false}>
+          <Login/>
+        </AuthLayout>)
+      },
+      {
+        path:'/signup',
+        element:(
+          <AuthLayout authentiaction={false}>
+            <Signup/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/all-posts',
+        element:(
+          <AuthLayout authentiaction>
+            {" "}
+            <AllPosts/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/add-posts',
+        element:(
+          <AuthLayout authentiaction>
+            {" "}
+            <AddPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/edit-post/:slug',
+        element:(
+          <AuthLayout>
+            {" "}
+            <EditPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/post/:slug',
+        element:(
+          <Post/>
+        )
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
 )
